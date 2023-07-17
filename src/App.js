c5f1d46d2c18354c8989fcd2ed99fadb7dbab54a
DavronMamaraimov <davronmamaraimov1996@gmail.com>
2023-07-17 08:56:02 +0500

    We are done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import React from "react";
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    state = {
+        count: 0,
+    };
+
+    plusOne = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    minusOne = () => {
+        this.setState({ count: this.state.count - 1 })
+    }
+
+    reset = () => {
+        this.setState({ count: 0 })
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <div className="counter-header"><h1>Counter</h1></div>
+                        <div className="counter-body"><p>{ this.state.count }</p></div>
+                        <div className="counter-footer d-flex">
+                            <button onClick={this.plusOne} className="btn btn-success">+</button>
+                            <button onClick={this.reset} className="btn btn-primary">Reset</button>
+                            <button onClick={this.minusOne} className="btn btn-danger">-</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
